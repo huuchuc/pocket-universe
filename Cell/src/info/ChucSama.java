55872151e01b79522eae4fe39d843326d8a161de
huuchuc <huuchuc7392@gmail.com>
2016-08-20 18:02:48 +0900

    create project

---
+++ Cell/src/info/ChucSama.java
@@ -0,0 +1,12 @@
+package info;
+
+public class ChucSama {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("My name is Chuc Nguyen");
+		System.out.println("I come from Vietnam");
+		System.out.println("This cell is created for sience perpose");
+	}
+
+}
---
+++ Cell/src/model/Cell.java
@@ -0,0 +1,16 @@
+package model;
+
+public class Cell {
+	
+	private int rebosome;
+	private int nuclear;
+       
+	public int getRebosome(){
+		return this.rebosome;
+	}
+	
+	public int getNuclear(){
+		return this.nuclear;
+	}
+	
+}
